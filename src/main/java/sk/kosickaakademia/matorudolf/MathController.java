3b5e5844bc006f5c09391ef27ebae35b85526459
Rudy439 <rudolf.mato@kosickaakademia.sk>
2021-03-10 10:56:12 +0100

    Streda hodina úprava

--- src/main/java/sk/kosickaakademia/matorudolf/MathController.java
+++ src/main/java/sk/kosickaakademia/matorudolf/MathController.java
@@ -1,8 +1,11 @@
 package sk.kosickaakademia.matorudolf;
 
-import net.minidev.json.JSONObject;
+
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,16 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class MathController {
 
     @PostMapping (path = "/add")
-    public String addTwoNumbers (@RequestBody String input) {
+      public ResponseEntity<String> addTwoNumbers(@RequestBody String input){
+    //public String addTwoNumbers (@RequestBody String input) {
         try {
             JSONObject object = (JSONObject) new JSONParser().parse(input);
             int a = Integer.parseInt(String.valueOf (object.get("a")));
             int b = Integer.parseInt(String.valueOf (object.get("b")));
             int result = a+b;
-            System.out.println(result);
+            //return"{\"result\":"+result+"}";
+            JSONObject res = new JSONObject();
+            res.put("result",result);
+
+            return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(res.toJSONString());
+
         } catch (ParseException e) {
             e.printStackTrace();
-        }
+        } catch (NumberFormatException e){
+            JSONObject obj = new JSONObject();
+            obj.put("error","Incorrect body of the request");
+          return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body((obj.toJSONString()));
+    }
         return null;
 
     }
